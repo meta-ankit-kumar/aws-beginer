6a08f094e0b2e6501b1723a56775525f41893daa
Ankit Kumar <ankit.kumar@financialforce.com>
2024-04-15 18:44:44 +0530

    Removed the logs

--- index.mjs
+++ index.mjs
@@ -9,17 +9,11 @@ const pool = mysql.createPool({
 });
 
 export const handler = async (event) => {
-	// Parse request body (assuming JSON format)
-	console.log('event.body', event);
 	const data = event.body;
-	console.log('process.env.DB_HOST', process.env.DB_HOST)
-	console.log('data', data);
-	// Prepare SQL statement using parameterized queries for security
 	const sql = 'INSERT INTO employee (firstName, lastName) VALUES ?';
 	const values = data.map((employee) => [employee.firstName, employee.lastName]);
 
 	try {
-		// Connect to database pool and execute query
 		await new Promise((resolve, reject) => {
 			pool.query(sql, [values], (err, results) => {
 				if (err) {
@@ -35,13 +29,11 @@ export const handler = async (event) => {
 			body: JSON.stringify({ message: 'Records inserted successfully!' }),
 		};
 	} catch (error) {
-		console.error('Hello in the catch', error);
 		return {
 			statusCode: 500,
 			body: JSON.stringify({ message: 'Internal server error occurred!' }),
 		};
 	} finally {
-		// Close connection pool (optional, handled automatically by some libraries)
-		// pool.end();
+		pool.end();
 	}
 };
