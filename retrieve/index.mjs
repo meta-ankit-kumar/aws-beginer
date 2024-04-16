0014d9b8170bb670b48e25868b52182d7212c76c
Ankit Kumar <ankit.kumar@financialforce.com>
2024-04-16 21:58:39 +0530

    Added the cloudwatch and s3 bucket logs

--- retrieve/index.mjs
+++ retrieve/index.mjs
@@ -1,4 +1,5 @@
 import * as mysql from 'mysql';
+import * as AWS from 'aws-sdk';
 
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
@@ -8,11 +9,36 @@ const pool = mysql.createPool({
 	port: process.env.DB_PORT
 });
 
-export const handler = async (event) => {
+const s3 = new AWS.S3();
+const cloudwatchlogs = new AWS.CloudWatchLogs();
+
+export const handler = async (event, context) => {
 	console.log('event', event);
 	const employeeId = event.pathParameters?.id;
 	const sql = `SELECT * FROM employee ${employeeId ? 'WHERE id = ?' : ''}`;
 	console.log('sql', sql);
+
+	const logData = {
+		httpMethod: event.httpMethod,
+		path: event.path
+	  };
+	
+	  await cloudwatchlogs.putLogEvents({
+		logGroupName: process.env.CLOUDWATCH_LOG_GROUP_NAME,
+		logStreamName: context.awsRequestId,
+		logEvents: [{
+		  message: JSON.stringify(logData),
+		  timestamp: Date.now(),
+		}]
+	  }).promise();
+	
+	  const params = {
+		Bucket: process.env.S3_LOG_BUCKET,
+		Key: `${context.awsRequestId}.log`,
+		Body: JSON.stringify(logData),
+	  };
+	
+	  await s3.putObject(params).promise();
 
 	try {
 		const employeeList = employeeId ? await new Promise((resolve, reject) => {
